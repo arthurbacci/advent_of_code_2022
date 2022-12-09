4e72aecffc25e8adc5657f3ffb836afb9b7a28fe
Arthur Bacci <arthurbacci@protonmail.com>
2022-12-09 15:36:21 -0300

    Day 9 part 2

--- src/day9.rs
+++ src/day9.rs
@@ -2,14 +2,27 @@ use std::io::{BufRead, BufReader};
 use std::fs::File;
 use std::collections::HashSet;
 
+fn follow(me: &mut (i32, i32), to_follow: (i32, i32)) {
+    if (me.1 - to_follow.1).abs() > 1 || (me.0 - to_follow.0).abs() > 1 {
+        if me.1 != to_follow.1  {
+            me.1 += if to_follow.1 > me.1 {1} else {-1};
+        }
+        if me.0 != to_follow.0 {
+            me.0 += if to_follow.0 > me.0 {1} else {-1};
+        }
+    }
+}
+
 pub fn main() {
     let f = File::open("src/day9.txt").unwrap();
     let reader = BufReader::new(f);
 
     let mut h_pos = (0i32, 0i32);
-    let mut t_pos = (0i32, 0i32);
+    let mut followers = [(0i32, 0i32); 9];
     let mut t_history = HashSet::new();
-    t_history.insert(t_pos);
+    let mut tail_history = HashSet::new();
+    t_history.insert(followers[0]);
+    tail_history.insert(followers[8]);
     
     let mut lines = reader.lines();
     let mut acc = 0u32;
@@ -36,19 +49,16 @@ pub fn main() {
             c => panic!("Invalid character {c:?}"),
         }
 
-        if (t_pos.1 - h_pos.1).abs() > 1 || (t_pos.0 - h_pos.0).abs() > 1 {
-            if t_pos.1 != h_pos.1  {
-                t_pos.1 += if h_pos.1 > t_pos.1 {1} else {-1};
-            }
-            if t_pos.0 != h_pos.0 {
-                t_pos.0 += if h_pos.0 > t_pos.0 {1} else {-1};
-            }
+        follow(&mut followers[0], h_pos);
+        for i in 1..9 {
+            let before = followers[i - 1];
+            follow(&mut followers[i], before);
         }
+        t_history.insert(followers[0]);
+        tail_history.insert(followers[8]);
 
-        t_history.insert(t_pos);
 
-
-        for y in -6..6 {
+        /*for y in -6..6 {
             for x in -6..6 {
                 if (x, y) == h_pos {
                     print!("H");
@@ -62,10 +72,10 @@ pub fn main() {
             }
             println!();
         }
-        println!();
+        println!();*/
     }
 
-    println!("{}", t_history.len());
+    println!("{} {}", t_history.len(), tail_history.len());
 }
 
 
