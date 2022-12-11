707f46b6e5e9328b3130e2745160b410f97b149f
Arthur Bacci <arthurbacci@protonmail.com>
2022-12-11 17:44:08 -0300

    Day 11 part 1

---
+++ src/day11.rs
@@ -0,0 +1,130 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::iter;
+use std::collections::VecDeque;
+
+struct Monkey {
+    items: VecDeque<u64>,
+    operation: Box<dyn Fn(u64) -> u64>,
+    divisible_by: u64,
+    throw_to: (usize, usize),
+}
+
+impl Monkey {
+    fn parse<T: Iterator<Item=String>>(lines: &mut T) -> Option<(usize, Monkey)> {
+        let first = lines.next()?;
+
+        let index: usize = first
+            .strip_prefix("Monkey ")?
+            .strip_suffix(":").unwrap()
+            .parse().unwrap();
+        
+        let items: VecDeque<u64> = lines.next().unwrap()
+            .strip_prefix("  Starting items: ").unwrap()
+            .split(", ")
+            .map(|x| x.parse().unwrap())
+            .collect();
+
+        let third_line = lines.next().unwrap();
+        let operation_str = third_line
+            .strip_prefix("  Operation: new = old ").unwrap();
+
+        let operation = operation_str.chars().next().unwrap();
+        let operand = operation_str[1..].strip_prefix(" ").unwrap();
+        let operation: Box<dyn Fn(u64) -> u64> = match operand {
+            "old" => match operation {
+                '*' => Box::new(|x| x * x),
+                '+' => Box::new(|x| x + x),
+                _ => panic!("Unknown operation {operation}"),
+            }
+            n => {
+                let n: u64 = n.parse().unwrap();
+                match operation {
+                    '*' => Box::new(move |x| x * n),
+                    '+' => Box::new(move |x| x + n),
+                    _ => panic!("Unknown operation {operation}"),
+                }
+            }
+        };
+
+        let divisible_by: u64 = lines.next().unwrap()
+            .strip_prefix("  Test: divisible by ").unwrap()
+            .parse().unwrap();
+        
+        let throw_to: (usize, usize) = (
+            lines.next().unwrap()
+                .strip_prefix("    If true: throw to monkey ").unwrap()
+                .parse().unwrap(),
+            lines.next().unwrap()
+                .strip_prefix("    If false: throw to monkey ").unwrap()
+                .parse().unwrap(),
+        );
+        
+
+        Some((
+            index,
+            Monkey {items, operation, divisible_by, throw_to},
+        ))
+    }
+
+    fn round(&mut self) -> Vec<(usize, u64)> {
+        let mut ret = Vec::new();
+
+        while let Some(mut item) = self.items.pop_front() {
+            item = (self.operation)(item);
+            item /= 3;
+            
+            ret.push((
+                if item % self.divisible_by == 0 {
+                    self.throw_to.0
+                } else {
+                    self.throw_to.1
+                },
+                item,
+            ));
+        }
+
+        ret
+    }
+}
+
+pub fn main() {
+    let file = File::open("src/day11.txt").unwrap();
+    let reader = BufReader::new(file);
+    let mut lines = reader.lines()
+        .map(|x| x.unwrap())
+        .chain(iter::once(" ".to_string()));
+
+    let mut monkeys = Vec::new();
+    while let Some((index, monkey)) = Monkey::parse(&mut lines) {
+        if index != monkeys.len() {
+            panic!("Index doesn't matches")
+        }
+        monkeys.push(monkey);
+
+        lines.next().unwrap();
+    }
+
+    let mut totals = vec![0; monkeys.len()];
+    for _ in 0..20 {
+        for i in 0..monkeys.len() {
+            let to_adds = monkeys[i].round();
+            totals[i] += to_adds.len();
+            for (i, el) in to_adds {
+                monkeys[i].items.push_back(el);
+            }
+        }
+    }
+
+    let mut maximums = (0, 0);
+    for i in totals {
+        if i > maximums.0 {
+            maximums = (i, maximums.0);
+        } else if i > maximums.1 {
+            maximums.1 = i;
+        }
+    }
+
+    println!("{}", maximums.0 * maximums.1);
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
 fn main() {
-    advent_of_code_2022::day10::spinoff::main();
-    advent_of_code_2022::day10::main();
+    advent_of_code_2022::day11::main();
 }
