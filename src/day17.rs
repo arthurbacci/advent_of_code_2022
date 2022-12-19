f27e6e8f66a0262485c418736f4c103d4404fe79
Arthur Bacci <arthurbacci@protonmail.com>
2022-12-19 16:50:14 -0300

    Comments on how to do part 2

--- src/day17.rs
+++ src/day17.rs
@@ -5,6 +5,13 @@ use std::collections::HashSet;
 // I need to store the maximums of each column and remove all the elements
 // which are below the minimum of the maximums
 
+// This approach didn't seem to work, now I will try to keep a list for each
+// column w/ a list of the N last and I will be pop-back-ing it (VecDeque) when
+// all the columns have the back element true, so that it will not tend to get
+// too big, but it will still be quite fast.
+// Also, don't forget to keep track of the number of values popped so that I
+// can index the `VecDeque`s
+
 #[derive(Debug, Clone, Copy)]
 enum RockType {
     Dash,
@@ -17,12 +24,12 @@ enum RockType {
 #[derive(Debug, Clone)]
 struct Rock {
     t: RockType,
-    p: (u64, u64),
+    p: (usize, usize),
 }
 
 impl Rock {
-    fn new(p: (u64, u64), t: RockType) -> Rock {Rock {t, p}}
-    fn pieces(&self) -> impl Iterator<Item=(u64, u64)> + '_ {
+    fn new(p: (usize, usize), t: RockType) -> Rock {Rock {t, p}}
+    fn pieces(&self) -> impl Iterator<Item=(usize, usize)> + '_ {
         match self.t {
             RockType::Dash  => vec![(0, 0), (1, 0), (2, 0), (3, 0)],
             RockType::Cross => vec![(1, 2), (0, 1), (1, 1), (2, 1), (1, 0)],
@@ -32,10 +39,10 @@ impl Rock {
         }.into_iter()
             .map(|(x, y)| (x + self.p.0, y + self.p.1))
     }
-    fn bottom(&self) -> u64 {
+    fn bottom(&self) -> usize {
         self.p.1
     }
-    fn top(&self) -> u64 {
+    fn top(&self) -> usize {
         self.bottom() + match self.t {
             RockType::Dash  => 0,
             RockType::Cross => 2,
@@ -44,10 +51,10 @@ impl Rock {
             RockType::Block => 1,
         }
     }
-    fn left(&self) -> u64 {
+    fn left(&self) -> usize {
         self.p.0
     }
-    fn right(&self) -> u64 {
+    fn right(&self) -> usize {
         self.left() + match self.t {
             RockType::Dash  => 3,
             RockType::Cross => 2,
@@ -57,7 +64,7 @@ impl Rock {
         }
     }
 
-    fn collides(&self, grid: &HashSet<(u64, u64)>) -> bool {
+    fn collides(&self, grid: &HashSet<(usize, usize)>) -> bool {
         for i in self.pieces() {
             if grid.get(&i).is_some() {
                 return true;
@@ -88,9 +95,10 @@ pub fn main() {
         }).cycle();
 
     let mut max = 0;
+    let mut maxes = [0; 7];
     let mut grid = HashSet::new();
 
-    for _ in 0 as usize..2022 {
+    for _ in 0 as usize..1_000_000 {
         let mut rock = Rock::new((2, max + 1), *rocks.next().unwrap());
 
         for _ in 0..3 {
@@ -128,7 +136,20 @@ pub fn main() {
         max = max.max(rock.top());
         for i in rock.pieces() {
             grid.insert(i);
+            if i.1 > maxes[i.0] {
+                maxes[i.0] = i.1;
+            }
         }
+        
+        let min_max = maxes.iter().min().unwrap();
+
+        let mut newgrid = HashSet::new();
+        for i in grid.drain() {
+            if i.1 + 10 >= *min_max {
+                newgrid.insert(i);
+            }
+        }
+        grid = newgrid;
     }
 
     println!("R: {max}");
